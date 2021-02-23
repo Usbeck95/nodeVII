c194c8c4d2d73bf3db73fbc404a12e5709dfc6d7
Carina <carinausbeck@live.dk>
2021-02-23 13:46:50 +0100

    ass.nodeVII.2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var confil = require('../views/continentsfil');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express about' });
+});
+
+router.get('/continents', function(req, res, next) {
+  res.render('continents', confil);
 });
 
 module.exports = router;
---
+++ views/continentsfil.js
@@ -0,0 +1,28 @@
+/* nml: continents of the world */
+"use strict";
+
+module.exports =  {
+    "confil" : [ 
+    {
+    "continent": "Africa"
+    },
+    {
+    "continent": "Antarctica"
+    },
+    {
+    "continent": "Asia"
+    },
+    {
+    "continent": "Europe"
+    },
+    {
+    "continent": "North America"
+    },
+    {
+    "continent": "Oceania"
+    },
+    {
+    "continent": "South America"
+    }
+]
+}
